c08c8db0cf22cec96236ce7cc07cfd23a8884599
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-21 17:32:07 -0700

    add basic folders

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import ItemControl from "./ItemControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <ItemControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
